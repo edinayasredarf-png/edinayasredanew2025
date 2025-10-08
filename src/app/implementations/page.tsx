9eb498a7ead35529ed7be511fba9c96ce7c851a7
alexnizelskiy <madnatec1@gmail.com>
2025-10-09 00:08:16 +0300

    текстовые правки2

--- src/app/implementations/page.tsx
+++ src/app/implementations/page.tsx
@@ -308,7 +308,7 @@ function SupportLogos() {
               }}
             >
               <Image src="/img/logos/asi.svg" alt="АСИ" width={120} height={24} className="h-6 w-auto shrink-0" />
-              <Image src="/img/logos/fasie.SVG" alt="ФСИ" width={120} height={24} className="h-6 w-auto shrink-0" />
+              <Image src="/img/logos/fasie.svg" alt="ФСИ" width={120} height={24} className="h-6 w-auto shrink-0" />
               <Image src="/img/logos/frii_logo.svg" alt="ФРИИ" width={120} height={24} className="h-6 w-auto shrink-0" />
               <Image src="/img/logos/mincifry.svg" alt="Минцифры" width={120} height={24} className="h-6 w-auto shrink-0" />
               <Image src="/img/logos/minstroy.svg" alt="Минстрой" width={120} height={24} className="h-6 w-auto shrink-0" />
