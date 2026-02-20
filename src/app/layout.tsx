1a8f62b11f6959ea3e99ea8ae38290e64fa5c495
alexnizelskiy <madnatec1@gmail.com>
2026-02-20 09:18:49 +0300

    код из битрикса

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { HashCleaner } from "@/components/HashCleaner";
 
 import Script from "next/script";
 import { SITE_CONFIG } from "@/lib/config";
+import { BitrixScript } from "./bitrix-script";
 
 // ========================================
 // ОПТИМИЗАЦИЯ ШРИФТОВ
@@ -255,19 +256,7 @@ export default function RootLayout({
 			{/* ========================================
     BITRIX24 CALL TRACKER
     ======================================== */}
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function(w,d,u){
-        var s=d.createElement('script');
-        s.async=true;
-        s.src=u+'?'+(Date.now()/60000|0);
-        var h=d.getElementsByTagName('script')[0];
-        h.parentNode.insertBefore(s,h);
-      })(window,document,'https://cdn-ru.bitrix24.ru/b32921504/crm/tag/call.tracker.js');
-    `,
-  }}
-/>
+<BitrixScript />
       </body>
     </html>
   );
