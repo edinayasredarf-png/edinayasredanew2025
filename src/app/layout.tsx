798aca53e4f77e686c387dde77210c7d4c43b4ba
alexnizelskiy <madnatec1@gmail.com>
2026-05-20 15:38:39 +0300

    установка пикселя вк

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -231,7 +231,44 @@ export default function RootLayout({
         />
 
 
+{/* ========================================
+    VK / MAIL.RU PIXEL
+    ======================================== */}
+<Script
+  id="vk-mailru-counter"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      var _tmr = window._tmr || (window._tmr = []);
+      _tmr.push({
+        id: "3767579",
+        type: "pageView",
+        start: (new Date()).getTime()
+      });
 
+      (function (d, w, id) {
+        if (d.getElementById(id)) return;
+
+        var ts = d.createElement("script");
+        ts.type = "text/javascript";
+        ts.async = true;
+        ts.id = id;
+        ts.src = "https://top-fwz1.mail.ru/js/code.js";
+
+        var f = function () {
+          var s = d.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(ts, s);
+        };
+
+        if (w.opera == "[object Opera]") {
+          d.addEventListener("DOMContentLoaded", f, false);
+        } else {
+          f();
+        }
+      })(document, window, "tmr-code");
+    `,
+  }}
+/>
 
         {/* ========================================
             GOOGLE ANALYTICS (GA4)
@@ -270,6 +307,16 @@ export default function RootLayout({
             />
           </div>
         </noscript>
+				{/* VK / MAIL.RU noscript */}
+<noscript>
+  <div>
+    <img
+      src="https://top-fwz1.mail.ru/counter?id=3767579;js=na"
+      style={{ position: 'absolute', left: '-9999px' }}
+      alt="Top.Mail.Ru"
+    />
+  </div>
+</noscript>
 				<HashCleaner />
         <TypographyNoWidows />
         <ModalProvider>
